57700bc585f4d53defcb17b7222de06d72953618
Franco Pignanelli <franqo1808@gmail.com>
2021-07-05 02:54:29 -0300

    Agrego el ultimo html

--- taller4/analisis.js
+++ taller4/analisis.js
@@ -1,54 +1,4 @@
-// const salariosCol = colombia.map(
-//   function(persona){
-//     return persona.salary;
-//   }
-// );
 
-// const salariosColSorted = salariosCol.sort(
-//   function(salaryA, salaryB){
-//     return salaryA - salaryB;
-//   }
-// );
-
-// function esPar(numero){
-//   return (numero % 2 === 0);
-// }
-
-// const calcularMediana = (arr) => {
-  
-//   let mitad = parseInt(arr.length / 2);
-//   let mediana;
-  
-//   arr.sort((a, b) => a - b); //ordeno el arr
-
-//   if ((arr.length % 2) === 0){//pregunto si es par
-//     const elemento1 = arr[mitad];
-//     const elemento2 = arr[mitad - 1];
-//     const promedioElemento1y2 = calcularPromedio([elemento1,elemento2]);
-//     mediana = promedioElemento1y2;
-  
-//   }else{
-//      mediana = arrr[mitad];
-//   }
-//   return mediana;
-// } 
-
-// //calculamos mediana general
-
-// function medianaSalarios(lista){
-//   const mitad = parseInt(lista.length / 2);
-
-//   if(esPar(lista.length)){
-//     const personaMitad1 = lista[mitad - 1];
-//     const personaMitad2 = lista[mitad];
-//     const mediana = calcularMediana([personaMitad1,personaMitad2]);
-//     return mediana;
-
-//   }else{
-//     const personaMitad = lista[mitad];
-//     return personaMitad;
-//   }
-// }
 function esPar(numerito) {
   return (numerito % 2 === 0);
 }
@@ -109,9 +59,6 @@ console.log({
   medianaGeneralCol,
   medianaTop10Col,
 });
-
-
-
 
 console.log(
   medianaSalarios(salariosColSorted)
